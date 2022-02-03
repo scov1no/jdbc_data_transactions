9fd292e91102f1ef2c5eb0c6c3014cc0119e1e00
DevEddi <scovinowork@gmail.com>
2022-02-03 09:25:20 -0300

    jdbc - transacao dados

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,34 +1,47 @@
 package application;
 
 import db.DB;
+import db.DbException;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Program {
     public static void main(String[] args) {
 
         Connection conn = null;
         Statement st = null;
-        ResultSet rs = null;
 
         try{
             conn = DB.getConnection();
 
+            conn.setAutoCommit(false);
+
             st = conn.createStatement();
 
-            rs = st.executeQuery("select * from department");
+            int rows1 = st.executeUpdate("update seller set BaseSalary = 2090 where DepartmentId = 1");
+            /*
+            int x =1;
+            if(x < 2){
+                throw new SQLException("Fake Error");
+            }*/
 
-            while(rs.next()){
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-            }
+            int rows2 = st.executeUpdate("update seller set BaseSalary = 3090 where DepartmentId = 2");
+
+            conn.commit();
+
+            System.out.println("rows1 " + rows1);
+            System.out.println("rows2 " + rows2);
+
+
         }catch(SQLException e){
-            e.printStackTrace();
+            try{
+                conn.rollback();
+                throw  new DbException("Transaction rolled back ! Cause  by: " + e.getMessage());
+            }catch (SQLException e1){
+                throw  new DbException("Erro trying rollback! Cause by: " + e1.getMessage());
+            }
         }
         finally {
-            DB.closeResultSet(rs);
             DB.closeStatement(st);
             DB.closeConnection();
         }
